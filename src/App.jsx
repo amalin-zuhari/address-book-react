ada68638cd10c1dc8e7551133cc9271a371e5b75
amalin-zuhari <amalinzuhari.work@gmail.com>
2024-11-16 00:04:39 +0800

    Set up React Router with routes for Contact List, Contact Details, Add New Contact, and Edit Contact

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import ContactList from './components/ContactList'
+import ContactDetails from './components/ContactDetails'
+import AddContact from './components/AddContact'
+import EditContact from './components/EditContact'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<ContactList />} />
+        <Route path="/contact/:id" element={<ContactDetails />} />
+        <Route path='/add' element={<AddContact />} />
+        <Route path='/edit/:id' element={<EditContact />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
